44d5347554463d5721a3edec70c8224e8fc8ff75
Jenny3779 <euni7727@gmail.com>
2023-03-19 15:29:49 +0900

    test

---
+++ test/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  test/Users/jenny/Desktop/work/test_xcode/test
+//
+//  Created by 지은 on 11/27/22.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text("Hello world!")
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 지은 on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainListView()
+        }
+    }
+}
