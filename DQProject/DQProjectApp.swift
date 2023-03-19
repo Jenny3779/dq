94af517e3a4e7885667d8f9e1ef242b5f3c25029
always_hyuk <never5686@naver.com>
2023-03-19 15:01:18 +0900

    Initial Commit

---
+++ DQProject/DQProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DQProjectApp.swift
+//  DQProject
+//
+//  Created by Sanghyuk Han on 2023/03/19.
+//
+
+import SwiftUI
+
+@main
+struct DQProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
